13e6d8547516e965b53aae9241d4a438fad68d4f
peytontettleton <36649600+peytontettleton@users.noreply.github.com>
2018-09-14 23:07:50 -0500

    Update

--- DogProject/DogProject/Program.cs
+++ DogProject/DogProject/Program.cs
@@ -6,7 +6,10 @@ namespace DogProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Keep the console window open in debug mode. Push key to exit.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
         }
     }
 }
